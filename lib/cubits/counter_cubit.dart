a4caae13fae2778e108dae7018234ee5caf5969d
Mohammed_Elsheikh <moh.reda198@gmail.com>
2023-12-05 21:34:56 +0200

    create cubit Function

--- lib/cubits/counter_cubit.dart
+++ lib/cubits/counter_cubit.dart
@@ -3,4 +3,18 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CounterCubit extends Cubit<CounterState> {
   CounterCubit(super.initialState);
+
+  int teamApoints = 0;
+
+  int teamBpoints = 0;
+
+  void incrementTeam({required String team, required int buttonNumber}) {
+    if (team == 'A') {
+      teamApoints += buttonNumber;
+      emit(IncrementTeamAState());
+    } else {
+      teamBpoints += buttonNumber;
+      emit(IncrementTeamBState());
+    }
+  }
 }
